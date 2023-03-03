c2b7219466364eefe1c7a507f9b6348f2ec26b10
miguelpalacios1987 <104658338+miguelpalacios1987@users.noreply.github.com>
2023-03-03 05:40:46 -0300

    Saida de dados

---
+++ JS_senac3/aula2.js
@@ -0,0 +1,9 @@
+import prompt from 'prompt-sync';
+let ler=prompt();
+
+console.log('Ola qual e seu nome');
+let pessoa=ler();
+
+console.log('seja vindo  '+ pessoa);
+
+
--- JS_senac3/boasvindas.js
+++ JS_senac3/boasvindas.js
@@ -1,3 +1,6 @@
+
+
+
 console.log('Ola seja vindo!');
 console.log('Tudo bem!');
 console.log('Ola seja vindo  \nTudo bem \nMeu bother');
@@ -5,3 +8,8 @@ console.log('Ola seja vindo  \nTudo bem \nMeu bother');
 console.log(`Oeee
 tudo 
 bem`);
+console.log(10);
+console.log(23+50);
+console.log(25*6);
+console.log(10,5+6,2);
+
